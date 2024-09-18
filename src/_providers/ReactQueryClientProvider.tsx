05bf607ef276a909127555b268937f88152396a1
shamsinator <shamsi.amir@gmail.com>
2024-09-18 14:42:24 +0200

    feat: setup React Query for state management

---
+++ src/_providers/ReactQueryClientProvider.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+
+interface ReactQueryClientProviderProps {
+  children: React.ReactNode;
+}
+
+/**
+ * A React Query client provider component that wraps the application with a QueryClient instance.
+ *
+ * @param {Object} props - The component props.
+ * @param {React.ReactNode} props.children - The child elements to be wrapped with the QueryClient provider.
+ * @return {JSX.Element} The QueryClientProvider component with the child elements.
+ */
+export const ReactQueryClientProvider = ({
+  children,
+}: ReactQueryClientProviderProps): JSX.Element => {
+  const [queryClient] = useState<QueryClient>(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
