a7c6c0c3e7af7715b8333e8e19edcac843c7f881
shamsinator <shamsi.amir@gmail.com>
2024-09-18 14:44:11 +0200

    feat: create and handle data for artwork display

---
+++ types.ts
@@ -0,0 +1,19 @@
+export type Artwork = {
+  id: string;
+  name: string;
+  year: number;
+  description: string;
+  source: string;
+  artist: {
+    image: string;
+    name: string;
+  };
+  images: {
+    thumbnail: string;
+    hero: {
+      small: string;
+      large: string;
+    };
+    gallery: string;
+  };
+};
