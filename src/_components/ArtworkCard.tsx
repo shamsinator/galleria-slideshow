9842dc811a6a3223d0ea744124ff591235e234cf
shamsinator <shamsi.amir@gmail.com>
2024-09-20 21:38:52 +0200

    style: adjust the hover effect on gallery items

--- src/_components/ArtworkCard.tsx
+++ src/_components/ArtworkCard.tsx
@@ -21,14 +21,21 @@ export default function ArtworkCard({
         tabIndex={0}
         href={encodeURI(`/gallery/${id}`)}
       >
-        <Image
-          className="w-full h-auto object-contain"
-          src={path.startsWith("/") ? path : `/${path}`} // Ensure the path starts with a leading slash
-          alt={name}
-          width={300}
-          height={400}
-        />
-        <div className="absolute top-0 w-full h-full bg-white/50 opacity-0 group-hover:opacity-100 transition-opacity"></div>
+        <div className="relative">
+          <Image
+            className="group-hover:opacity-65 transition-all object-cover"
+            src={path.startsWith("/") ? path : `/${path}`} // Ensure the path starts with a leading slash
+            alt={name}
+            width={300}
+            height={400}
+            sizes="100vw"
+            style={{
+              width: "100%",
+              height: "auto",
+            }}
+          />
+          <div className="absolute top-0 bottom-0 w-full h-full bg-gradient-to-t from-black via-transparent to-transparent" />
+        </div>
         <div className="absolute left-5 bottom-5 flex flex-col gap-1 text-white">
           <h3 className="text-xl font-bold break-words max-w-[220px]">
             {name}
