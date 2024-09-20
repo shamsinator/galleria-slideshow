c43da2c74bc0a4e2a5bdcacff4abeb0ba2ccbee6
shamsinator <shamsi.amir@gmail.com>
2024-09-20 21:53:11 +0200

    feat: implement loader indicator
    this is a temporary to mask the flicker effect when gallery items are loaded

--- src/_components/ArtworkGallery.tsx
+++ src/_components/ArtworkGallery.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import PuffLoader from "react-spinners/PuffLoader";
 import { Artwork } from "../../types";
-import { paintings } from "../app/data";
-import ArtworkCard from "./ArtworkCard";
+import ArtworkCard from "@/_components/ArtworkCard";
+import { paintings } from "@/app/data";
 
 type MasonryGridProps = {
   paintings: Artwork[];
@@ -10,7 +13,27 @@ type MasonryGridProps = {
 /**
  * A React component that displays a masonry layout of painting images.
  */
-const MasonryGrid: React.FC<MasonryGridProps> = ({ paintings }) => {
+const MasonryGrid = ({ paintings }: MasonryGridProps) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating a delay to show the loading spinner
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  // Show loader while data is loading
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center">
+        <PuffLoader color="#36D7B7" size={150} />
+      </div>
+    );
+  }
+
   // If there are no paintings, show a message
   if (!paintings || paintings.length === 0) {
     return <p>No paintings available.</p>;
@@ -18,23 +41,38 @@ const MasonryGrid: React.FC<MasonryGridProps> = ({ paintings }) => {
 
   // Create a masonry layout from the paintings
   return (
-    <div className="masonry">
-      {paintings.map(({ id, images, name, artist }) => (
-        <div key={id} className="masonry-item">
-          {/* Each masonry item contains an ImageCard component */}
-          <ArtworkCard
-            id={id}
-            name={name}
-            artist={artist.name}
-            path={images.thumbnail}
-          />
+    <div className="relative">
+      {/* Overlay - shown while loading */}
+      {isLoading && (
+        <div className="absolute inset-0 flex justify-center items-center bg-gray-900 bg-opacity-50 z-10">
+          <PuffLoader color="#36D7B7" size={150} />
         </div>
-      ))}
+      )}
+
+      {/* Masonry grid */}
+      <div
+        className={`masonry ${
+          isLoading ? "opacity-0" : "opacity-100"
+        } transition-opacity duration-700`}
+      >
+        {paintings.map(({ id, images, name, artist }) => (
+          <div key={id} className="masonry-item relative">
+            <ArtworkCard
+              id={id}
+              name={name}
+              artist={artist.name}
+              path={images.thumbnail}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 export const getStaticProps = async () => {
+  const paintings = await fetchPaintingsData();
+
   return {
     props: {
       paintings,
@@ -42,5 +80,10 @@ export const getStaticProps = async () => {
     revalidate: 3600,
   };
 };
+
+// Mock function to simulate fetching data (replace with real fetching logic later on this point)
+async function fetchPaintingsData() {
+  return paintings; // TODO: Replace with real API call or data fetching logic
+}
 
 export default MasonryGrid;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import MainHeader from "@/_components/MainHeader";
 import { paintings } from "./data";
 import ArtworkGallery from "@/_components/ArtworkGallery";
-import { Fragment } from "react";
 
 export default function Home() {
   return (
-    <div className="py-4 px-4 md:px-5 2xl:px-12 ">
+    <div className="py-4 px-4 md:px-5 2xl:px-12">
       <header className="mx-auto flex flex-row items-center justify-between max-w-[1190px] min-h-[120px] h-[14.2vh] border-b border-gray-300 w-full">
         <MainHeader />
       </header>
