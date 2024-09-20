19eb0fba661f29bdd8676e5205309a3ce0bf7134
shamsinator <shamsi.amir@gmail.com>
2024-09-20 21:41:41 +0200

    style: Add responsive padding to main container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ import { Fragment } from "react";
 
 export default function Home() {
   return (
-    <Fragment>
+    <div className="py-4 px-4 md:px-5 2xl:px-12 ">
       <header className="mx-auto flex flex-row items-center justify-between max-w-[1190px] min-h-[120px] h-[14.2vh] border-b border-gray-300 w-full">
         <MainHeader />
       </header>
       <main className="max-w-[1190px] mx-auto mt-10">
         <ArtworkGallery paintings={paintings} />
       </main>
-    </Fragment>
+    </div>
   );
 }
