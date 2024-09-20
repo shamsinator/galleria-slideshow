a657fbe0084255d87e2b1e11e9a39c9d57c91fc6
shamsinator <shamsi.amir@gmail.com>
2024-09-20 11:28:03 +0200

    feat: correction for an important visual design aspect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ import { Fragment } from "react";
 export default function Home() {
   return (
     <Fragment>
-      <header className="mx-auto flex flex-row items-center justify-between max-w-[1360px] min-h-[120px] h-[14.2vh] border-b border-gray-300 w-full">
+      <header className="mx-auto flex flex-row items-center justify-between max-w-[1190px] min-h-[120px] h-[14.2vh] border-b border-gray-300 w-full">
         <MainHeader />
       </header>
-      <main className="max-w-[1360px] mx-auto mt-10">
+      <main className="max-w-[1190px] mx-auto mt-10">
         <ArtworkGallery paintings={paintings} />
       </main>
     </Fragment>
