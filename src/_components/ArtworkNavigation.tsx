813e9f0877f381ae9ac01f75392b87d1b5a481b4
shamsinator <shamsi.amir@gmail.com>
2024-09-26 14:20:31 +0200

    feat: improve this component a little bit

--- src/_components/ArtworkNavigation.tsx
+++ src/_components/ArtworkNavigation.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useMemo } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -12,53 +12,58 @@ interface SlideFooterProps {
 }
 
 const ArtworkNavigation = ({ paintings, currentIndex }: SlideFooterProps) => {
-  const [isLast, setIsLast] = useState(false);
+  const isLastArtwork = currentIndex === paintings.length - 1;
   const [progress, setProgress] = useState(0);
   const intervalRef = useRef<NodeJS.Timer | null>(null);
   const router = useRouter();
 
   useEffect(() => {
-    setIsLast(currentIndex === paintings.length - 1);
     setProgress(0);
 
-    if (intervalRef.current) {
-      // ignore ESLint
-      clearInterval(intervalRef.current);
-    }
-
-    // Set a new interval for navigation
+    // @ts-ignore
     intervalRef.current = window.setInterval(() => {
       setProgress((prev) => {
-        if (prev < 100) {
-          return prev + 1;
-        } else {
-          // When progress is full, navigate to the next artwork
-          if (currentIndex < paintings.length - 1) {
-            router.push(`/gallery/${paintings[currentIndex + 1].id}`); // Navigate to the next artwork
+        const nextProgress = prev + 1;
+        if (nextProgress >= 100) {
+          // @ts-ignore
+          clearInterval(intervalRef.current!);
+          if (!isLastArtwork) {
+            router.push(`/gallery/${paintings[currentIndex + 1].id}`);
           }
-          return prev;
+          return prev; // Keep progress full after navigation
         }
+        return nextProgress;
       });
     }, 60);
 
     return () => {
       if (intervalRef.current) {
+        // @ts-ignore
         clearInterval(intervalRef.current);
       }
     };
-  }, [currentIndex, paintings, router]);
+  }, [currentIndex, isLastArtwork, paintings, router]);
 
-  // Get the next and previous artwork
-  const prevPainting = currentIndex > 0 ? paintings[currentIndex - 1] : null;
-  const nextPainting =
-    currentIndex < paintings.length - 1 ? paintings[currentIndex + 1] : null;
+  // Memoize these values so they are only recalculated when currentIndex or paintings changes. This reduces unnecessary recomputations on re-renders.
+  const prevPainting = useMemo(
+    () => (currentIndex > 0 ? paintings[currentIndex - 1] : null),
+    [currentIndex, paintings]
+  );
+
+  const nextPainting = useMemo(
+    () =>
+      currentIndex < paintings.length - 1 ? paintings[currentIndex + 1] : null,
+    [currentIndex, paintings]
+  );
 
   return (
     <footer className="flex z-50 flex-col justify-between items-center w-full fixed bottom-0 left-0 px-2 lg:px-10 py-3 bg-white 2xl:px-12">
       {/* Progress Bar */}
       <div className="w-full bg-gray-200 h-1 mb-4">
         <div
-          className={`h-full ${isLast ? "bg-black" : "bg-gray-500"}`}
+          className={`h-full ${
+            isLastArtwork ? "bg-black" : "bg-gray-500"
+          } progress-bar`}
           style={{ width: `${progress}%` }}
         ></div>
       </div>
@@ -73,7 +78,10 @@ const ArtworkNavigation = ({ paintings, currentIndex }: SlideFooterProps) => {
       <nav className="flex justify-between">
         {/* Previous Button */}
         {prevPainting && (
-          <Link href={`/gallery/${prevPainting.id}`}>
+          <Link
+            href={`/gallery/${prevPainting.id}`}
+            aria-label="Go to previous artwork"
+          >
             <button className="px-4 py-2 text-white hover:opacity-50">
               <Image
                 src="/assets/images/icon-back-button.svg"
@@ -87,7 +95,10 @@ const ArtworkNavigation = ({ paintings, currentIndex }: SlideFooterProps) => {
 
         {/* Next Button (disabled if it's the last painting) */}
         {nextPainting ? (
-          <Link href={`/gallery/${nextPainting.id}`}>
+          <Link
+            href={`/gallery/${nextPainting.id}`}
+            aria-label="Go to next artwork"
+          >
             <button className="px-4 py-2 text-white hover:opacity-50">
               <Image
                 src="/assets/images/icon-next-button.svg"
@@ -98,7 +109,11 @@ const ArtworkNavigation = ({ paintings, currentIndex }: SlideFooterProps) => {
             </button>
           </Link>
         ) : (
-          <button className="px-4 py-2 bg-gray-400 text-white" disabled>
+          <button
+            className="px-4 py-2 bg-gray-400 text-white"
+            disabled
+            aria-disabled="true"
+          >
             <Image
               src="/assets/images/icon-next-button.svg"
               alt="next icon"
